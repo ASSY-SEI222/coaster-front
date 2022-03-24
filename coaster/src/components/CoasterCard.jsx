50a8ce8a439716070716d76cde4b37b2aa12ac80
Yusong Shi <jackshi.sys@gmail.com>
2022-03-24 13:09:08 -0400

    CoasterCard

--- coaster/src/components/CoasterCard.jsx
+++ coaster/src/components/CoasterCard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function CoasterCard() {
+function CoasterCard(props) {
   return (
-    <div>CoasterCard</div>
+    <div className="card Coaster-card" >
+    <div className="img-wrapper">
+      <img src={props.image} alt={props.name}></img>
+    </div>
+    <div className="info-wrapper flex-col">
+      <h3>{props.name}</h3>
+      <p>{props.rating}</p>
+    </div>
+  </div>
   )
 }
 
