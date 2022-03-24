17fe893894eae74d09cb863aeed9ce085f7c1e52
Steven DeLitta <sdelitta@gmail.com>
2022-03-24 13:21:08 -0400

    completed test park and coaster cards

--- coaster/src/components/ParkCards.jsx
+++ coaster/src/components/ParkCards.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 function ParkCards(props) {
   return (
-    <li className="park-item" id={props.name}>
+    <div className="card Coaster-card" >
+      <li className="park-item" id={props.name}>
         <h3>Name: {props.name}</h3>
         <h5>Location: {props.capitol}</h5>
       </li>
+      <div className="img-wrapper">
+        <img src={props.image} alt={props.name}></img>
+      </div>
+      <div className="info-wrapper flex-col">
+        <h3>{props.name}</h3>
+        <p>{props.rating}</p>
+      </div>
+    </div>
+    
   )
 }
 
