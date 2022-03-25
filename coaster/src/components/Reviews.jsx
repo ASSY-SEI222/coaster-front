98a25dd916319012b1e42e9657a4d1e7213b61b5
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-03-24 21:17:21 -0400

    fixed misc red flags

--- coaster/src/components/Reviews.jsx
+++ coaster/src/components/Reviews.jsx
@@ -6,27 +6,22 @@ function Reviews() {
   const [fEmail, setEmail] = useState("")
 
   const getDetails = () => {
-alert(fEmail)    
-return {
-      name: fName,
-      comments: fEmail,
-      rating: 320457
+  return {
+        name: fName,
+        comments: fEmail,
+        rating: 320457
 
-    }
- }
+      }
+  }
 
- const form = async () => {
-  const details = getDetails()
-//   const something = await axios.post(`/api/coasters/review/623cc3a36266b13db7757bf6`, {details})
-//   .then(function () {
-//     alert('got here');
-// })
-// .catch(function (error) {
-//     alert(error);
-// });
-  const something = axios.get(`127.0.0.1:3001/api/allCoasters`)  
-  alert(something.details)
-  // .coasters[0].reviews[2].name)
+  const sendToAPI = async (e) => {
+      const details = getDetails()        
+      
+      // const something = await axios.post(`http://127.0.0.1:3001/api/coasters/review/623cb3b8d6168b0bed6a5176`,details)  
+      const something = await axios.get(`http://127.0.0.1:3001/api/allCoasters`)  
+
+      e.preventDefault()  
+      console.log(something.coasters)
  }
 
   return ( 
@@ -35,29 +30,27 @@ return {
         <h3>Please write your review here</h3>
 
         <form id="feedback" action="">
-          <div class="pinfo">Your personal info</div>
+          <div className="pinfo">Your personal info</div>
   
-              <div class="form-group">
-              <div class="col-md-4 inputGroupContainer">
-              <div class="input-group">
-              <span class="input-group-addon"><i class="fa fa-user"></i></span>
-              <input  name="name" placeholder="John Doe" class="form-control"  type="text" onChange={(e) => setName(e.target.value)}/>
+              <div className="form-group">
+              <div className="col-md-4 inputGroupContainer">
+              <div className="input-group">
+              <span className="input-group-addon"><i className="fa fa-user"></i></span>
+              <input  name="name" placeholder="John Doe" className="form-control"  type="text" onChange={(e) => setName(e.target.value)}/>
           </div>
              </div>
           </div>
             
-          <div class="form-group">
-            <div class="col-md-4 inputGroupContainer">
-            <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-              <input name="email" type="text" class="form-control" placeholder="john.doe@yahoo.com" onChange={(e) => setEmail(e.target.value)} />
-              <button onClick={()=> form()}>Cleek</button>
-               </div>
+          <div className="form-group">
+            <div className="col-md-4 inputGroupContainer">
+            <div className="input-group">
+              <span className="input-group-addon"><i className="fa fa-envelope"></i></span>
+              <input name="comment" type="text" className="form-control" placeholder="I love this ride!" onChange={(e) => setEmail(e.target.value)} />
+              <button onClick={()=> sendToAPI()}>Submit</button>
+            </div>
             </div>
           </div>
-   </form>
-   {/* {console.log('this is a log')} */}
-
+        </form>
 </div>
   )
 }
